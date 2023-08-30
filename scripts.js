24f0d3436fbe5b5c26d633ed8dc31f07d66558d9
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-30 13:18:27 -0400

    added labels for x and y axes and edited their styles

--- scripts.js
+++ scripts.js
@@ -17,6 +17,7 @@ const setOptions = (data) => {
 
 // draw the bar chart using provided data, setOptions, and element to render to
 const drawBarChart = (data, options, element) => {
+  // template to add correct number of bars and their values
   const barsTemplate = data.map((bar) => {
     // get percentage of each bar compared to max value for height
     const barHeight = (bar / options.height) * 100;
@@ -25,15 +26,29 @@ const drawBarChart = (data, options, element) => {
     return `<div class="chart-bar mid" style="height:${barHeight}%; width:calc(${barWidth}% - 15px)"><span class="chart-bar--value">${bar}</span></div>`;
     // add join to remove rendering commas
   }).join("");
-
+  const yVals = [];
+  const markerSpacing = options.height / data.length;
+  for (let i = 0; i <= options.height; i = i + markerSpacing){
+    yVals.push(i);
+  }
+  // chart template to bring all the elements together
   const chartTemplate = `<div class="chart-container--inner">
-    <div class="chart-yaxis"></div>
+    <div class="chart-yaxis">
+      ${yVals.map((y) => {
+        return `<span>${y}</span>`
+      }).join("")}
+    </div>
     <div class="chart-bars">
       ${barsTemplate}
     </div>
-    <div class="chart-xaxis"></div>
+    <div class="chart-xaxis">
+      ${data.map((x) => {
+        return `<span>${x}</span>`
+      }).join("")}
+    </div>
   </div>`;
 
+  // add the chart to the specified DOM element
   $(element).append(chartTemplate);
 };
 
