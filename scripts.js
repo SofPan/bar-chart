c0eb6f2f60a744f36810bb7326547ffa773d27b9
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-30 11:12:44 -0400

    placeholder text and chart rendering area, some guiding comments for build in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+const DUMMY_DATA = [1, 2, 3, 4, 5];
+const DUMMY_OPTIONS = {
+  height: 3,
+  width: 5
+}
+const DUMMY_ELEMENT = $('#chart');
+/*
+  Parameters:
+  data - the data the chart will work with
+  options - an object that has options for the chart, such as width and height
+  element - the DOM element that the chart will be rendered into
+*/
+const drawBarChart = (data, options, element) => {
+  console.log(data, options, element);
+};
+
+// document ready
 $(() => {
-  console.log('hello world');
+
 })
