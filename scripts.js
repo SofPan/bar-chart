44c525f234859129ed1f514f79c0965cac044d85
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-30 12:21:01 -0400

    created template to render bars in chart

--- scripts.js
+++ scripts.js
@@ -1,20 +1,43 @@
 const DUMMY_DATA = [1, 2, 3, 4, 5];
-const DUMMY_OPTIONS = {
-  height: 3,
-  width: 5
-}
-const DUMMY_ELEMENT = $('#chart');
-/*
-  Parameters:
-  data - the data the chart will work with
-  options - an object that has options for the chart, such as width and height
-  element - the DOM element that the chart will be rendered into
-*/
+const CHART_ELEMENT = "#chart";
+
+// chart width and height are dependent on the data being passed in
+const setOptions = (data) => {
+  let width = data.length;
+  let height = data.sort((a, b) => {
+    return b > a;
+  });
+
+  height = height[height.length - 1];
+  return {
+    width: width,
+    height: height,
+  };
+};
+
+// draw the bar chart using provided data, setOptions, and element to render to
 const drawBarChart = (data, options, element) => {
-  console.log(data, options, element);
+  const barsTemplate = data.map((bar) => {
+    // get percentage of each bar compared to max value for height
+    const barHeight = (bar / options.height) * 100;
+    // get percentage of width for each bar
+    const barWidth = 100 / options.width;
+    return `<div class="chart-bar" style="height:${barHeight}%; width:calc(${barWidth}% - 15px)"></div>`;
+    // add join to remove rendering commas
+  }).join("");
+
+  const chartTemplate = `<div class="chart-container--inner">
+    <div class="chart-yaxis"></div>
+    <div class="chart-bars">
+      ${barsTemplate}
+    </div>
+    <div class="chart-xaxis"></div>
+  </div>`;
+
+  $(element).append(chartTemplate);
 };
 
 // document ready
 $(() => {
-
-})
+  drawBarChart(DUMMY_DATA, setOptions(DUMMY_DATA), CHART_ELEMENT);
+});
